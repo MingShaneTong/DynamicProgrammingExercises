660bf7aff37b9755959d4796107f037a5c85570c
Ming Shane Tong <mstong800@gmail.com>
2025-05-26 21:34:55 +1200

    use array for problem 1

--- 01. 0-1 Knapsack Problem/solution.py
+++ 01. 0-1 Knapsack Problem/solution.py
@@ -1,3 +1,5 @@
+import numpy as np
+from numpy.typing import ArrayLike
 from typing import List
 from helper import Item
 
@@ -6,23 +8,23 @@ def solution(capacity: int, items: List[Item]) -> List[Item]:
     optimal_table = populate_optimal_value_table(capacity, sorted_items)
     return backtrace_items(capacity, sorted_items, optimal_table)
 
-def populate_optimal_value_table(capacity: int, sorted_items: List[Item]) -> List[List[int]]:
-    optimal_values_table = [[0 for c in range(capacity + 1)]]
+def populate_optimal_value_table(capacity: int, sorted_items: List[Item]) -> ArrayLike:
+    optimal_values_table = np.zeros((len(sorted_items) + 1, capacity + 1))
+
     # add each item into the pool of items to add
-    for item in sorted_items:
-        item_optimal_table = [0]
+    for i in range(1, len(sorted_items) + 1):
+        item = sorted_items[i - 1]
+
         # find the maximum value at the new capacity given items
         for c in range(1, capacity + 1):
-            optimal_without_item = optimal_values_table[-1][c]
-            optimal_with_item = item.value + optimal_values_table[-1][c - item.weight] if item.weight <= c else optimal_without_item
+            optimal_without_item = optimal_values_table[i - 1][c]
+            optimal_with_item = item.value + optimal_values_table[i - 1][c - item.weight] if item.weight <= c else optimal_without_item
             optimal_value = max(optimal_without_item, optimal_with_item)
-            item_optimal_table.append(optimal_value)
-
-        optimal_values_table.append(item_optimal_table)
+            optimal_values_table[i][c] = optimal_value
     
     return optimal_values_table
 
-def backtrace_items(capacity: int, sorted_items: List[Item], optimal_table: List[List[int]]) -> List[Item]:
+def backtrace_items(capacity: int, sorted_items: List[Item], optimal_table: ArrayLike) -> List[Item]:
     pointer_capacity = capacity
     pointer_row = len(sorted_items)
     items_included = []
